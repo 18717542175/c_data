b28301288dc17abbd6664d5c61b79b55dacbca3d
liugang <2637681295@qq.com>
2023-01-31 19:09:25 +0800

    单链表实现

--- SbList.c
+++ SbList.c
@@ -2,6 +2,7 @@
 #include "SbList.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 
 SLTNode* BuyListNode(SbListType x)
@@ -81,7 +82,109 @@ void SbListPopBack(SLTNode** phead)//从尾部删除数据
 }
 void SbListPopFront(SLTNode** phead)  //头部删除
 {
+	if (*phead == NULL)
+	{
+		return;
+	}else {
+		SLTNode* next = (*phead)->next;  //保存第二个结点的地址
 
+		free(*phead);
+		*phead = next;
+	}  
+}
+
+void SbListInsert(SLTNode** phead, SLTNode* pos, SbListType x)//在某个位置插入数据
+{
+	//assert(*phead != NULL);
+	//
+	//SLTNode* curnode = *phead;
+	//SLTNode* nextnode = (*phead)->next;
+
+	////创造一个新结点
+	//SLTNode* newnode = (SLTNode*)malloc(sizeof(SLTNode));
+	//newnode->data = x;
+	//newnode->next = NULL;
+	//
+
+
+	////找到目标结点
+	//while (curnode->next != NULL)
+	//{
+	//	if (curnode == pos)
+	//	{
+	//		curnode->next = newnode;
+	//		newnode->next = nextnode;
+	//	}
+	//	else
+	//	{
+	//		curnode = curnode->next;
+	//		nextnode = curnode->next;
+	//	}  
+	//}
+
+	SLTNode* newnode = BuyListNode(x);
+
+	//找到pos的前一个位置
+	SLTNode* posPrev = *phead;
+	while (posPrev->next != pos)
+	{
+		posPrev = pos->next;
+	}
+	posPrev->next = newnode;
+	newnode->next = pos;
+}
+SLTNode* SbListFind(SLTNode* phead, SbListType x) //查找数据
+{
+	//遍历链表
+	SLTNode* node = phead;
+	while (node->next != NULL)
+	{
+		if (node->data == x)
+			return node;
+		else
+			node = node->next;
+	}
+	return NULL; 
+}
+void SbListEase(SLTNode** phead, SLTNode* pos)
+{
+	if (*phead == pos)
+	{
+		*phead = pos->next;
+		free(pos);
+	}
+	else {
+		SLTNode* prev = *phead;
+		while (prev->next != pos)
+		{
+			prev = prev->next;
+		}
+
+		prev->next = pos->next;
+		free(pos);
+		pos = NULL;
+	}
+}
+void SbListDeastory(SLTNode** phead)//销毁链表
+{
+	assert(phead);
+
+	SLTNode* cur = *phead;
+	while (cur != NULL)
+	{
+		SLTNode* next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
+void SbListInsertAfert(SLTNode* pos, SbListType x)
+{
+	SLTNode* newnode = BuyListNode(x);
+
+	//先将新结点的next指向pos后的一个结点的next
+	//再将pos的next指向newnode
+	newnode->next = pos->next;
+	pos->next = newnode;
 }
 
 
--- SbList.h
+++ SbList.h
@@ -14,3 +14,8 @@ void SbListPushBack(SLTNode** phead, SbListType x); //从尾部插入数据
 void SbListPushFront(SLTNode** phead, SbListType x);  //头部插入
 void SbListPopBack(SLTNode** phead); //从尾部删除数据
 void SbListPopFront(SLTNode** phead);  //头部删除
+void SbListInsert(SLTNode** phead, SLTNode* pos, SbListType x);//在某个位置插入数据
+SLTNode* SbListFind(SLTNode* phead, SbListType x); //查找数据
+void SbListEase(SLTNode** phead, SLTNode* pos); //删除数据
+void SbListDeastory(SLTNode** phead); //销毁链表
+void SbListInsertAfert(SLTNode* pos, SbListType x);	//在pos之后插入数据
